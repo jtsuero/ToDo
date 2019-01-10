8bd18e47c1ae78dc367b12cf406301cdc672cf59
Jordan Suero <jtsuero@Jordans-MacBook-Pro.local>
2019-01-09 21:39:05 -0800

    Added delete function

--- app.js
+++ app.js
@@ -3,10 +3,9 @@
 
 class ToDo {
 		constructor() {
-				this.list = ["butts", "booty"];
+				this.list = ["yo", "wassup", "hello"];
 		}
 
-		
 
 		getList() {
 				return this.list;
@@ -17,23 +16,47 @@ class ToDo {
 		}
 
 		removeItem() {
-
+				//how to delete a particular task, that may not be the first in array
 		}
 }
 let list1 = new ToDo();
-let item1 = prompt("Enter your first item");
-list1.addItem(item1);
-let textArea = document.getElementById('body');
+document.getElementById("submitbutton").addEventListener("click",addItem);
+document.getElementById("deleteBtn").addEventListener("click", deleteItem);
+let x = document.createElement("OL");
+x.setAttribute("id", "myList");
+console.log(list1.getList());
 
-function printList(list) {
-	let newItem = document.createElement("li");
-	let node;
-	let newList = document.getElementById("body");	
-	for (let i = 0; i < list.length; i++) {
-		node = document.createTextNode(list[i]);
-		newItem.appendChild(node);
-		newList.insertBefore(newItem,null);
-	} 
+function printList(list, action) {
+		let node;
+		let newList = document.getElementById("body");
+		let starting = null;
+		if (action === "adding") {
+			starting = list.length - 1;
+		} else {
+			starting = 0;
+		}
+				for(let i= starting; i < list.length; i++) {
+						document.body.appendChild(x);
+						let newItem = document.createElement("li");
+						node = document.createTextNode(list[i]);
+						newItem.appendChild(node);
+						x.insertBefore(newItem,null);
+				}				
+}
+function addItem() {	
+		let newItem = document.getElementById("item").value;
+		list1.addItem(newItem);
+
+		printList(list1.getList(), "adding");
+		console.log(list1.getList());
 }
 
-printList(list1.getList());
+function deleteItem() {
+		let deleteItem = document.getElementById("delete").value;
+		console.log(list1.getList());
+		console.log(deleteItem);
+		list1.getList().splice(deleteItem- 1, 1);		
+		console.log(list1.getList());
+		printList(list1.getList(), "deleting");
+
+}
