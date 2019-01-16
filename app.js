7e9b4e245288a0d35f1f688197acb80a24f903ff
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2019-01-15 22:08:40 -0800

    added checkboxes and fixed delete functionality

--- app.js
+++ app.js
@@ -2,61 +2,76 @@
 //Jordan Suero
 
 class ToDo {
-		constructor() {
-				this.list = ["yo", "wassup", "hello"];
-		}
+  constructor() {
+    this.list = [];
+    this.name = "default";
+  }
 
+  getList() {
+    return this.list;
+  }
 
-		getList() {
-				return this.list;
-		}
-
-		addItem(item) {
-				this.list.push(item);
-		}
-
-		removeItem() {
-				//how to delete a particular task, that may not be the first in array
-		}
+  addItem(item) {
+    this.list.push(item);
+  }
 }
-let list1 = new ToDo();
+//Setting up the list and the form for entries and deletions
 document.getElementById("submitbutton").addEventListener("click",addItem);
-document.getElementById("deleteBtn").addEventListener("click", deleteItem);
-let x = document.createElement("OL");
-x.setAttribute("id", "myList");
-console.log(list1.getList());
+document.getElementById("deleteBtn").addEventListener("click", findDeleteItem);
+let myList = document.createElement("div");
+console.log(myList);
+myList.setAttribute("id", "myList");
+let list1 = new ToDo();
 
-function printList(list, action) {
-		let node;
-		let newList = document.getElementById("body");
-		let starting = null;
-		if (action === "adding") {
-			starting = list.length - 1;
-		} else {
-			starting = 0;
-		}
-				for(let i= starting; i < list.length; i++) {
-						document.body.appendChild(x);
-						let newItem = document.createElement("li");
-						node = document.createTextNode(list[i]);
-						newItem.appendChild(node);
-						x.insertBefore(newItem,null);
-				}				
+function makeList() {
+  list1.name = "jordan"
+  console.log(list1.name);
 }
+
+function printList(list) {
+  let node;
+  let newList = document.getElementById("body");
+  for(let i= list.length - 1; i < list.length; i++) {
+    document.body.appendChild(myList);
+    let lineItem = document.createElement("span");
+    lineItem.innerHTML = " " +  list[i];
+    let newLine = document.createElement("br");
+    let checkBox = document.createElement("input");
+    checkBox.type =  "checkbox";
+    myList.appendChild(checkBox);
+    myList.appendChild(lineItem);
+    myList.appendChild(newLine);
+  }				
+}
+
 function addItem() {	
-		let newItem = document.getElementById("item").value;
-		list1.addItem(newItem);
-
-		printList(list1.getList(), "adding");
-		console.log(list1.getList());
+  let newItem = document.getElementById("item").value;
+  list1.addItem(newItem);
+  printList(list1.getList());
+  console.log(list1.getList());
 }
 
-function deleteItem() {
-		let deleteItem = document.getElementById("delete").value;
-		console.log(list1.getList());
-		console.log(deleteItem);
-		list1.getList().splice(deleteItem- 1, 1);		
-		console.log(list1.getList());
-		printList(list1.getList(), "deleting");
+//finds item in list to delete
+  function findDeleteItem() {
+    let currentList = document.getElementById("myList");
+    let children = currentList.childNodes;
+    for(let i = 0; i < children.length; i++) {
+      if(children[i].checked) {
+        console.log(i)
+        deleteItem(children[i+1])
+        console.log(children[i]);
+        currentList.removeChild(children[i]);
+        currentList.removeChild(children[i]);
+        currentList.removeChild(children[i]);
+      }
+    }
+  }
 
+//deletes item from list array
+function deleteItem(item) {
+  let deleteItem = list1.getList().indexOf(item);
+  console.log(list1.getList());
+  (list1.getList().splice(deleteItem, 1));	
+  //eraseList(deleteItem - 1);
 }
+
